d764d5f09d307c6e7e12acd9c1ebacd20e967f7a
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-17 00:59:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
     DB[:conn].execute(sql, x)
   end
 
-  def self.first_student_in_grade_10(x)
+  def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT *
       FROM students
